1a2c90befd97636fc466af300ca325f6e296e122
David Danko <dcdanko@gmail.com>
2018-01-11 14:47:54 -0500

    dist breaking, got rid of kmer filter

--- src/fragment_db.jl
+++ src/fragment_db.jl
@@ -8,60 +8,42 @@ end
 
 
 type FragDB
-    k::Int
     l::Int
     max_rep_target_dist::Int
-    kmers_to_reps::Dict{DNAKmer, Array{DNASequence}}
     reps_to_fragments::Dict{DNASequence, Array{Fragment}}
 end
 
 function create_fragment_db(l::Int, max_rep_target_dist::Int) :: FragDB
-    k = floor((l - max_rep_target_dist) / (max_rep_target_dist + 1))
-    db = FragDB(k, 
-                l, 
+    db = FragDB(l, 
                 max_rep_target_dist, 
                 Dict{DNAKmer{k}, Array{DNASequence}}(), 
                 Dict{DNASequence, Array{DNASequence}}())
     return db
 end
 
-function hamming_dist(a::DNASequence, b::DNASequence) :: Int
+function hamming_dist(a::DNASequence, b::DNASequence, thresh::Int) :: Bool
     d = 0
     for i in 1:length(a)
         d += a[i] != b[i] ? 1 : 0 
+        if d > thresh
+            return false
+        end
     end
-    return d
+    return d <= thresh
 end
 
 function add_fragment_to_db!(db::FragDB, frag::DNASequence, fraginfo::String)
     fragObj = Fragment(frag, fraginfo)
     matched = false
-    kmers = map(x -> x[2], each(DNAKmer{db.k}, frag))
-    for kmer in kmers
-        for rep in get(db.kmers_to_reps, kmer, [])
-            d = hamming_dist(frag, rep)
-            if d <= db.max_rep_target_dist
-                push!(db.reps_to_fragments[rep], fragObj)
-                matched = true
-                break
-            end
-        end
-        if matched
+    for rep in keys(db.reps_to_fragments)
+        if hamming_dist(frag, rep, db.max_rep_target_dist)
+            push!(db.reps_to_fragments[rep], fragObj)
+            matched = true
             break
         end
     end
 
-
     if !matched
-        for kmer in kmers
-            try
-                push!(db.kmers_to_reps[kmer], frag)
-            catch error
-                if isa(error, KeyError)
-                    db.kmers_to_reps[kmer] = [frag]
-                end
-            end
-        end
         db.reps_to_fragments[frag] = [fragObj]
     end
 end
@@ -80,8 +62,7 @@ function write_db_to_file(db::FragDB, fname::String)
         end
         push!(j, obj)
     end
-    obj = Dict("k" => db.k,
-               "l" => db.l,
+    obj = Dict("l" => db.l,
                "max_rep_target_dist" => db.max_rep_target_dist,
                "clusters" => j)  
     iof = open(fname, "w")
